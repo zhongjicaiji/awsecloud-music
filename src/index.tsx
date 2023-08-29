8907064e9b1f57a1de129ff56e5c53e75f4ebcd6
zhongjicaiji <1565769040@qq.com>
2023-08-29 21:30:23 +0800

    完善轮播图和修改框

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
 
+//设置移动端适配
+document.documentElement.style.fontSize=100/750 +"vw"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+ <Router>
+   <App />
+ </Router>
  
-    <App />
-
 );
 
 
